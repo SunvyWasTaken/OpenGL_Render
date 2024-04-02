4189079f851db86c81bc374bc31817ec5ff1d2cc
DevOfSomewhere <keanu.vongue@gmail.com>
2024-04-02 20:19:18 +0200

    OpenGL work
    Ok ct une erreur à la con mb

--- DefaultApp/Sources/Main/main.cpp
+++ DefaultApp/Sources/Main/main.cpp
@@ -1,14 +1,9 @@
 // SunvyNeverExist Copyright please don't steal work that isn't yours
 
-#include "Core/Public/CoreMinimal.h"
-
 #include <iostream>
 #include <memory>
 
-using namespace Lite;
-
 int main()
 {
-	std::unique_ptr<Application> CurrentApp = std::make_unique<Application>();
-	CurrentApp->Run();
+	
 };
