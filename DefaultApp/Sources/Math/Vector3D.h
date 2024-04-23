e39aee160ab844b9b620f27182f0b0ad1ecb04b8
NysoS <k.ledoux.dev@gmail.com>
2024-04-23 23:20:03 +0200

    #8 fix vector3D

--- DefaultApp/Sources/Math/Vector3D.h
+++ DefaultApp/Sources/Math/Vector3D.h
@@ -5,21 +5,21 @@ namespace Math
 	template <typename VectorType>
 	struct Vector3D
 	{
-		Vector3D(const VectorType& x_, const VectorType& y_);
+		Vector3D(const VectorType& x_, const VectorType& y_, const VectorType& z_);
 		Vector3D(const Vector3D& vector);
 
-		VectorType x, y;
+		VectorType x, y, z;
 	};
 
 	template <typename VectorType>
-	Vector3D<VectorType>::Vector3D(const VectorType& x_, const VectorType& y_)
-		: x(x_), y(y_)
+	Vector3D<VectorType>::Vector3D(const VectorType& x_, const VectorType& y_, const VectorType& z_)
+		: x(x_), y(y_), z(z_)
 	{
 	}
 
 	template <typename VectorType>
 	Vector3D<VectorType>::Vector3D(const Vector3D& vector)
-		: x(vector.x), y(vector.y)
+		: x(vector.x), y(vector.y), z(vector.z)
 	{
 	}
 }
