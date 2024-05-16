66b781c510a30c4b23937cae49cf6daa7ee5b968
NysoS <k.ledoux.dev@gmail.com>
2024-05-16 19:25:51 +0200

    quick refacto

--- DefaultApp/Sources/LowLevel_Renderer/Primitive/Plane.h
+++ DefaultApp/Sources/LowLevel_Renderer/Primitive/Plane.h
@@ -1,50 +1,39 @@
 #pragma once
 
 #include "LowLevel_Renderer/Primitive/Vertex.h"
-#include "LowLevel_Renderer/Shader/Shader.h"
 #include "PrimitiveUtils.h"
-#include "LowLevel_Renderer/Texture/Texture.h"
 #include "Math/Matrix.h"
 #include "Math/Transform.h"
 
 #include <array>
 #include <glad/glad.h>
 
+#include "PrimitiveMesh.h"
+
 template <typename T>
-class Plane
+class Plane : public PrimitiveMesh<T>
 {
 	using vertex_type = Vertex<T>;
+	using Transform = Math::Transform<T>;
+	using parent = PrimitiveMesh<T>;
 
 public:
 	Plane()
-		: transform(Math::Transform<T>()), m_vao(0), m_vbo(0), m_shaderProgram(0), m_texture(Texture{})
+		: parent()
 	{
-		load();
 	}
 
 	~Plane()
 	{
-		glDeleteVertexArrays(1, &m_vao);
-		glDeleteBuffers(1, &m_vbo);
-		glDeleteBuffers(1, &m_ebo);
-		glDeleteProgram(m_shaders->program);
-
-		if(m_shaders)
-		{
-			delete m_shaders;
-			m_shaders = nullptr;
-		}
 	}
 
 	void load()
 	{
-		m_texture = Texture("Ressources\\sc.png", GL_TEXTURE0);
-
 		std::array<vertex_type, 4> vertices = {
-			vertex_type( { -1.f,	0.0f,	 1.f }, { 1.0f,	1.0f,	1.0f }, {  0.0f,		0.0f }, { 0.f, 1.f, 0.f} ),
-			vertex_type( { -1.f,	0.0f,	-1.f }, { 1.0f,	1.0f,	1.0f }, {  0.0f,		1.0f }, { 0.f, 1.f, 0.f}),
-			vertex_type( { 1.f,	0.0f,	-1.f }, { 1.0f,	1.0f,	1.0f }, {  1.0f,		1.0f }, { 0.f, 1.f, 0.f}),
-			vertex_type( { 1.f,	0.0f,	 1.f }, { 1.0f,	1.0f,	1.0f }, {  1.0f,		0.0f }, { 0.f, 1.f, 0.f})
+			vertex_type( { -1.f,	0.0f,	-1.f }, { 1.0f,	1.0f,	1.0f }, {  0.0f,		0.0f }, { 0.f, 1.f, 0.f}),
+			vertex_type( { -1.f,	0.0f,	 1.f }, { 1.0f,	1.0f,	1.0f }, {  0.0f,		1.0f }, { 0.f, 1.f, 0.f}),
+			vertex_type( { 1.f,	0.0f,	 1.f }, { 1.0f,	1.0f,	1.0f }, {  1.0f,		1.0f }, { 0.f, 1.f, 0.f}),
+			vertex_type( { 1.f,	0.0f,	-1.f }, { 1.0f,	1.0f,	1.0f }, {  1.0f,		0.0f }, { 0.f, 1.f, 0.f})
 		};
 
 		std::array<GLuint, 6> indices = {
@@ -52,67 +41,16 @@ public:
 			2, 3, 0
 		};
 
-		LOAD_VERTEX_ARRAYS(m_vao)
-		LOAD_ARRAY_BUFFER(m_vbo, vertices)
-		LOAD_ELEMENT_ARRAY_BUFFER(m_ebo, indices)
+		LOAD_ARRAY_BUFFER(this->m_vbo, vertices)
+		LOAD_ELEMENT_ARRAY_BUFFER(this->m_ebo, indices)
 
-		ShaderInfo shaders[] = {
-			{GL_VERTEX_SHADER,  "default.vert"},
-			{GL_FRAGMENT_SHADER, "default.frag"},
-			{GL_NONE, nullptr}
-		};
-
-		m_shaders = Shader::loadShader(shaders);
-		glUseProgram(m_shaders->program);
-
-		// /!\ Attention, ca marche que si t = float, -> dommage
-		LOAD_BASIC_VERTEX_ATTRIB_POINTER()
-
-		glBindVertexArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-		m_texture.bind();
-		m_texture.textUnit(m_shaders->program, "tex0");
+		parent::load();
 	}
 
-	void render(const ContextRenderer& contextRenderer, Cube<T>& light, Camera& camera)
+	void render(ContextRenderer& contextRenderer)
 	{
-		glUseProgram(m_shaderProgram);
-		glBindVertexArray(m_vao);
+		parent::render(contextRenderer);
 
-		/*const GLuint projectionLocation = glGetUniformLocation(m_shaderProgram, "projection");
-		glUniformMatrix4fv(projectionLocation, 1, 0, contextRenderer.projection.data());
-		const GLuint viewLocation = glGetUniformLocation(m_shaderProgram, "view");
-		glUniformMatrix4fv(viewLocation, 1, 0, contextRenderer.view.data());
-
-		const GLuint modelLocation = glGetUniformLocation(m_shaderProgram, "model");
-		glUniformMatrix4fv(modelLocation, 1, 0, transform.getMatrix().data());
-
-		const GLuint lightColorLocation = glGetUniformLocation(m_shaderProgram, "lightColor");
-		glUniform4fv(lightColorLocation, 1, reinterpret_cast<float*>(&light.color));
-
-		const GLuint lightPositionLocation = glGetUniformLocation(m_shaderProgram, "lightPosition");
-		glUniform4fv(lightPositionLocation, 1, reinterpret_cast<float*>(&light.transform.position));
-
-		const GLuint CameraPositionLocation = glGetUniformLocation(m_shaderProgram, "viewPosition");
-		glUniform4fv(CameraPositionLocation, 1, reinterpret_cast<float*>(&camera.transform.position));*/
-
-		m_texture.bind();
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
-
-	void update()
-	{
-	}
-
-	Math::Transform<T> transform;
-
-private:
-	GLuint m_vao;
-	GLuint m_vbo;
-	GLuint m_ebo;
-	GLuint m_shaderProgram;
-	Shader* m_shaders;
-	Texture m_texture;
 };
